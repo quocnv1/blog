3789d5d4ecc1ca2de1b508a24ca2d16bff1c04f4
quocnv <quoc120600@gmail.com>
2021-12-27 22:45:49 +0700

    OK

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./dist/code'))
+
+app.get('/*',(req,res)=>
+    res.sendFile('index.html', {root:'dist/angular-heroku/'}),
+
+);
+
+app.listen(process.env.PORT||8080)
